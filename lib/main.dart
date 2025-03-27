9f40f0c3907eb9bec644f1e14dd6b210be4b1140
Shiroyasha1997 <valenzueladiego1997@gmail.com>
2025-03-27 08:59:18 -0300

    se agregaron las funciones principales para el funcionamiento de la app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather_icons/weather_icons.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +11,249 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        fontFamily: 'Roboto', // Usamos la fuente Roboto
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const WeatherPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _WeatherPageState createState() => _WeatherPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _WeatherPageState extends State<WeatherPage> {
+  final TextEditingController _cityController = TextEditingController();
+  String _weather = '';
+  String _temperature = '';
+  String _windSpeed = '';
+  String _feelsLike = '';
+  String _sunrise = '';
+  String _sunset = '';
+  String _rainChance = '';
+  String _weatherIconUrl = '';
+  bool _isLoading = false;
+  bool _isLocationPermissionGranted = false;
+  Color _backgroundColor = Colors.blue;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _checkLocationPermission();
+  }
+
+  Future<void> _checkLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      setState(() {
+        _weather = 'Los servicios de ubicación están deshabilitados.';
+      });
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        setState(() {
+          _weather = 'Permiso de ubicación denegado.';
+        });
+        return;
+      }
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isLocationPermissionGranted = true;
     });
+  }
+
+  Future<Position?> _getCurrentLocation() async {
+    if (_isLocationPermissionGranted) {
+      return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+    }
+    return null;
+  }
+
+  Future<void> _getWeather() async {
+    setState(() {
+      _isLoading = true;
+      _weather = '';
+      _temperature = '';
+      _windSpeed = '';
+      _feelsLike = '';
+      _sunrise = '';
+      _sunset = '';
+      _rainChance = '';
+      _backgroundColor = Colors.blue; // Resetear el fondo por defecto
+      _weatherIconUrl = ''; // Resetear el ícono
+    });
+
+    final apiKey = '2c578e426a2c4e4bb3f234043252603'; // 🔥 Reemplaza con tu API Key
+    String city = _cityController.text.trim();
+
+    if (city.isEmpty && _isLocationPermissionGranted) {
+      // Si no se proporciona una ciudad, obtenemos la ubicación actual
+      Position? position = await _getCurrentLocation();
+      if (position != null) {
+        city = '${position.latitude},${position.longitude}';
+      } else {
+        setState(() {
+          _isLoading = false;
+          _weather = 'Error al obtener la ubicación.';
+        });
+        return;
+      }
+    }
+
+    final url = Uri.parse(
+        'https://api.weatherapi.com/v1/current.json?key=$apiKey&q=$city&aqi=no&lang=es'); // Cambié lang=es para obtener en español
+
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        print(data);  // Imprime la respuesta completa para verificar los datos
+
+        if (data['location'] != null && data['current'] != null) {
+          setState(() {
+            _isLoading = false;
+
+            // Asignamos los nuevos valores
+            _weather = '${data['location']['name']}, ${data['location']['region']}, ${data['location']['country']} - ${data['current']['condition']['text']}';
+            _temperature = '${data['current']['temp_c']}°C';
+            _windSpeed = '${data['current']['wind_kph']} km/h';
+            _feelsLike = '${data['current']['feelslike_c']}°C';
+
+            // Verificar si la clave 'astro' existe antes de acceder a 'sunrise' y 'sunset'
+            if (data['current']['astro'] != null) {
+              _sunrise = data['current']['astro']['sunrise'] ?? 'Desconocido';
+              _sunset = data['current']['astro']['sunset'] ?? 'Desconocido';
+            } else {
+              _sunrise = 'No disponible';
+              _sunset = 'No disponible';
+            }
+
+            _rainChance = '${data['current']['precip_mm']} mm';
+
+            // Asignamos el ícono
+            _weatherIconUrl = 'https:${data['current']['condition']['icon']}';
+          });
+        } else {
+          setState(() {
+            _isLoading = false;
+            _weather = 'Ciudad no encontrada o datos inválidos.';
+          });
+        }
+      } else {
+        setState(() {
+          _isLoading = false;
+          _weather = 'Error: ${response.reasonPhrase}';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+        _weather = 'Fallo al obtener los datos del clima. Error: $e';
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Weather App'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        color: _backgroundColor,
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            TextField(
+              controller: _cityController,
+              decoration: const InputDecoration(
+                labelText: 'Ingresa el nombre de la ciudad',
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _getWeather,
+              child: const Text('Obtener clima'),
+            ),
+            const SizedBox(height: 20),
+            _isLoading
+                ? const CircularProgressIndicator()
+                : Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _weatherIconUrl.isEmpty
+                        ? const CircularProgressIndicator()
+                        : Image.network(
+                      _weatherIconUrl,
+                      width: 50,
+                      height: 50,
+                    ),
+                    const SizedBox(width: 20),
+                    Text(
+                      _temperature,
+                      style: const TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  _weather,
+                  style: const TextStyle(fontSize: 24),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  '🌡️ Sensación térmica: $_feelsLike',
+                  style: const TextStyle(fontSize: 18),
+                ),
+                Text(
+                  '💨 Velocidad del viento: $_windSpeed',
+                  style: const TextStyle(fontSize: 18),
+                ),
+                Text(
+                  '🌅 Amanecer: $_sunrise',
+                  style: const TextStyle(fontSize: 18),
+                ),
+                Text(
+                  '🌇 Atardecer: $_sunset',
+                  style: const TextStyle(fontSize: 18),
+                ),
+                Text(
+                  '🌧️ Probabilidad de lluvia: $_rainChance',
+                  style: const TextStyle(fontSize: 18),
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
