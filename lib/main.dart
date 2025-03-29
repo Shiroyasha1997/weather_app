b943319086996e8b619af672a7f47f8adf23391c
Shiroyasha1997 <valenzueladiego1997@gmail.com>
2025-03-29 00:11:13 -0300

    se agrego una mejora en el cuadro de texto para que muestre sugerencias de ciudades al escribir en el.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -33,6 +34,7 @@ class WeatherPage extends StatefulWidget {
 
 class _WeatherPageState extends State<WeatherPage> {
   final TextEditingController _cityController = TextEditingController();
+  Timer? _debounce;
   String _weather = '';
   String _temperature = '';
   String _windSpeed = '';
@@ -43,11 +45,36 @@ class _WeatherPageState extends State<WeatherPage> {
   bool _isLoading = false;
   bool _isLocationPermissionGranted = false;
   Color _backgroundColor = Colors.blue.shade100;
+  List<String> _suggestedCities = [];
+  bool _showSuggestions = false;
 
   @override
   void initState() {
     super.initState();
     _checkLocationPermission();
+    _cityController.addListener(_onCityChanged);
+  }
+
+  @override
+  void dispose() {
+    _cityController.removeListener(_onCityChanged);
+    _cityController.dispose();
+    _debounce?.cancel();
+    super.dispose();
+  }
+
+  void _onCityChanged() {
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(seconds: 1), () {
+      if (_cityController.text.isNotEmpty) {
+        _getCitySuggestions(_cityController.text);
+      } else {
+        setState(() {
+          _suggestedCities = [];
+          _showSuggestions = false;
+        });
+      }
+    });
   }
 
   Future<void> _checkLocationPermission() async {
@@ -87,23 +114,16 @@ class _WeatherPageState extends State<WeatherPage> {
     return null;
   }
 
-  Future<void> _getWeatherByCity() async {
+  Future<void> _getWeatherByCity(String city) async {
     setState(() {
       _isLoading = true;
-      _weather = '';
-      _temperature = '';
-      _windSpeed = '';
-      _feelsLike = '';
-      _rainChance = '';
-      _humidity = '';
-      _backgroundColor = Colors.blue.shade100;
-      _weatherIconUrl = '';
+      _resetWeatherData();
     });
 
     final apiKey = '2c578e426a2c4e4bb3f234043252603';
-    String city = _cityController.text.trim();
+    String cityName = city.trim();
 
-    if (city.isEmpty) {
+    if (cityName.isEmpty) {
       setState(() {
         _isLoading = false;
         _weather = 'Por favor, ingresa una ciudad.';
@@ -112,32 +132,14 @@ class _WeatherPageState extends State<WeatherPage> {
     }
 
     final url = Uri.parse(
-        'https://api.weatherapi.com/v1/current.json?key=$apiKey&q=$city&aqi=no&lang=es');
+        'https://api.weatherapi.com/v1/current.json?key=$apiKey&q=$cityName&aqi=no&lang=es');
 
     try {
       final response = await http.get(url);
 
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
-
-        if (data['location'] != null && data['current'] != null) {
-          setState(() {
-            _isLoading = false;
-            _weather =
-            '${data['location']['name']}, ${data['location']['region']}, ${data['location']['country']} - ${data['current']['condition']['text']}';
-            _temperature = '${data['current']['temp_c']}°C';
-            _windSpeed = '${data['current']['wind_kph']} km/h';
-            _feelsLike = '${data['current']['feelslike_c']}°C';
-            _rainChance = '${data['current']['precip_mm']} mm';
-            _humidity = '${data['current']['humidity']}%';
-            _weatherIconUrl = 'https:${data['current']['condition']['icon']}';
-          });
-        } else {
-          setState(() {
-            _isLoading = false;
-            _weather = 'Ciudad no encontrada o datos inválidos.';
-          });
-        }
+        _updateWeatherData(data);
       } else {
         setState(() {
           _isLoading = false;
@@ -152,67 +154,92 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
+  Future<void> _getCitySuggestions(String query) async {
+    final apiKey = '2c578e426a2c4e4bb3f234043252603';
+    final url = Uri.parse(
+        'https://api.weatherapi.com/v1/search.json?key=$apiKey&q=$query');
+
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        List<String> cities = data.map<String>((city) => '${city['name']}, ${city['region']}, ${city['country']}').toList();
+
+        setState(() {
+          _suggestedCities = cities;
+          _showSuggestions = true;
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _suggestedCities = [];
+        _showSuggestions = false;
+      });
+    }
+  }
+
   Future<void> _getWeatherByLocation() async {
     setState(() {
       _isLoading = true;
-      _weather = '';
-      _temperature = '';
-      _windSpeed = '';
-      _feelsLike = '';
-      _rainChance = '';
-      _humidity = '';
-      _backgroundColor = Colors.blue.shade100;
-      _weatherIconUrl = '';
+      _resetWeatherData();
     });
 
-    Position? position = await _getCurrentLocation();
-    if (position != null) {
-      final apiKey = '2c578e426a2c4e4bb3f234043252603';
-      final url = Uri.parse(
-          'https://api.weatherapi.com/v1/current.json?key=$apiKey&q=${position.latitude},${position.longitude}&aqi=no&lang=es');
-
-      try {
-        final response = await http.get(url);
-
-        if (response.statusCode == 200) {
-          final data = json.decode(response.body);
-
-          if (data['location'] != null && data['current'] != null) {
-            setState(() {
-              _isLoading = false;
-              _weather =
-              '${data['location']['name']}, ${data['location']['region']}, ${data['location']['country']} - ${data['current']['condition']['text']}';
-              _temperature = '${data['current']['temp_c']}°C';
-              _windSpeed = '${data['current']['wind_kph']} km/h';
-              _feelsLike = '${data['current']['feelslike_c']}°C';
-              _rainChance = '${data['current']['precip_mm']} mm';
-              _humidity = '${data['current']['humidity']}%';
-              _weatherIconUrl = 'https:${data['current']['condition']['icon']}';
-            });
-          } else {
-            setState(() {
-              _isLoading = false;
-              _weather = 'No se pudo obtener el clima en tu ubicación.';
-            });
-          }
-        } else {
-          setState(() {
-            _isLoading = false;
-            _weather = 'Error: ${response.reasonPhrase}';
-          });
-        }
-      } catch (e) {
-        setState(() {
-          _isLoading = false;
-          _weather = 'Fallo al obtener los datos del clima. Error: $e';
-        });
-      }
-    } else {
+    final position = await _getCurrentLocation();
+    if (position == null) {
       setState(() {
         _isLoading = false;
-        _weather = 'Error al obtener la ubicación.';
+        _weather = 'No se pudo obtener la ubicación.';
+      });
+      return;
+    }
+
+    final apiKey = '2c578e426a2c4e4bb3f234043252603';
+    final url = Uri.parse(
+        'https://api.weatherapi.com/v1/current.json?key=$apiKey&q=${position.latitude},${position.longitude}&aqi=no&lang=es');
+
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        _updateWeatherData(data);
+      } else {
+        setState(() {
+          _isLoading = false;
+          _weather = 'Error: ${response.reasonPhrase}';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+        _weather = 'Fallo al obtener los datos del clima. Error: $e';
       });
     }
+  }
+
+  void _updateWeatherData(Map<String, dynamic> data) {
+    setState(() {
+      _isLoading = false;
+      _weather =
+      '${data['location']['name']}, ${data['location']['region']}, ${data['location']['country']} - ${data['current']['condition']['text']}';
+      _temperature = '${data['current']['temp_c']}°C';
+      _windSpeed = '${data['current']['wind_kph']} km/h';
+      _feelsLike = '${data['current']['feelslike_c']}°C';
+      _rainChance = '${data['current']['precip_mm']} mm';
+      _humidity = '${data['current']['humidity']}%';
+      _weatherIconUrl = 'https:${data['current']['condition']['icon']}';
+    });
+  }
+
+  void _resetWeatherData() {
+    _weather = '';
+    _temperature = '';
+    _windSpeed = '';
+    _feelsLike = '';
+    _rainChance = '';
+    _humidity = '';
+    _weatherIconUrl = '';
   }
 
   @override
@@ -238,11 +265,24 @@ class _WeatherPageState extends State<WeatherPage> {
               textAlign: TextAlign.center,
               style: const TextStyle(fontSize: 18),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _getWeatherByCity,
-              child: const Text('Obtener clima por ciudad'),
-            ),
+            if (_showSuggestions)
+              Container(
+                height: 200,
+                child: ListView.builder(
+                  itemCount: _suggestedCities.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(_suggestedCities[index]),
+                      onTap: () {
+                        _getWeatherByCity(_suggestedCities[index]);
+                        setState(() {
+                          _showSuggestions = false;
+                        });
+                      },
+                    );
+                  },
+                ),
+              ),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: _getWeatherByLocation,
@@ -253,27 +293,12 @@ class _WeatherPageState extends State<WeatherPage> {
                 ? const CircularProgressIndicator()
                 : Column(
               children: [
-                _weatherIconUrl.isEmpty
-                    ? const SizedBox.shrink()
-                    : Image.network(
-                  _weatherIconUrl,
-                  width: 80,
-                  height: 80,
-                ),
+                if (_weatherIconUrl.isNotEmpty)
+                  Image.network(_weatherIconUrl, width: 80, height: 80),
                 const SizedBox(height: 15),
-                Text(
-                  _temperature,
-                  style: const TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+                Text(_temperature, style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold)),
                 const SizedBox(height: 15),
-                Text(
-                  _weather,
-                  style: const TextStyle(fontSize: 20, color: Colors.black54),
-                  textAlign: TextAlign.center,
-                ),
+                Text(_weather, style: const TextStyle(fontSize: 20, color: Colors.black54), textAlign: TextAlign.center),
                 const SizedBox(height: 30),
                 _weatherDetails(),
               ],
@@ -296,12 +321,6 @@ class _WeatherPageState extends State<WeatherPage> {
   }
 
   Widget _buildDetailRow(String label, String value) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5),
-      child: Text(
-        '$label $value',
-        style: const TextStyle(fontSize: 18),
-      ),
-    );
+    return Padding(padding: const EdgeInsets.symmetric(vertical: 5), child: Text('$label $value', style: const TextStyle(fontSize: 18)));
   }
 }
